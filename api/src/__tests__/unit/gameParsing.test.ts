45b1195f144519002d0e435c1e941dda5280b754
W Biggs <wilson@wilsonbiggs.com>
2022-02-21 20:49:39 -0500

    Adding parsing functions for team stats and scores

--- api/src/__tests__/unit/gameParsing.test.ts
+++ api/src/__tests__/unit/gameParsing.test.ts
@@ -1,4 +1,4 @@
-import { parseHomeAway } from '../../tasks/parseGameThread';
+import { parseHomeAway, parseTeamStats, parseTeamScores } from '../../tasks/parseGameThread';
 import mockGameJSON from './mockGameJSON.json';
 
 test('finds home/away with records and rankings', () => {
@@ -20,4 +20,34 @@ test('finds home/away with no records or rankings', () => {
     homeTeam: 'Kent State',
     awayTeam: 'Ball State'
   })
+});
+
+test('gets stats from S3 thread', () => {
+  expect(parseTeamStats(mockGameJSON.selftext)).toEqual({
+    home: {
+      passYds: 19,
+      rushYds: 328,
+      interceptions: 0,
+      fumbles: 0,
+      fgAttempts: 0,
+      fgMakes: 0,
+      timeOfPossession: 1114
+    },
+    away: {
+      passYds: 247,
+      rushYds: 124,
+      interceptions: 1,
+      fumbles: 1,
+      fgAttempts: 2,
+      fgMakes: 2,
+      timeOfPossession: 566
+    },
+  });
+});
+
+test('gets scores from S3 thread', () => {
+  expect(parseTeamScores(mockGameJSON.selftext)).toEqual({
+    home: [2, 22, 14, 7],
+    away: [14, 6, 7, 7],
+  });
 });
--- api/src/tasks/parseGameThread.ts
+++ api/src/tasks/parseGameThread.ts
@@ -11,6 +11,57 @@ const parseHomeAway = function parseHomeAway(gameTitle: string) {
   return false;
 };
 
+const parseTeamStats = function parseTeamStats(gameSelfText: string) {
+  const statsStringsRegex = /Total Passing Yards.+?\n.+?\n(.+?)\n/gm;
+  let statsStringsMatch;
+  let stats = {
+    home: {},
+    away: {}
+  }
+
+  let matchNo = 0;
+  while ((statsStringsMatch = statsStringsRegex.exec(gameSelfText)) !== null && matchNo < 2) {
+    const [passYds, rushYds,, interceptions, fumbles, fgs, timeOfPossession,] = statsStringsMatch[1].split('|');
+    const splitFGs = fgs.split('/').map(val => parseInt(val, 10));
+    const splitTOP = timeOfPossession.split(':').map(val => parseInt(val, 10));
+    const topSeconds = (splitTOP[0] * 60) + splitTOP[1];
+    const teamStats = {
+      rushYds: parseInt(rushYds, 10),
+      passYds: parseInt(passYds, 10),
+      interceptions: parseInt(interceptions, 10),
+      fumbles: parseInt(fumbles, 10),
+      fgAttempts: splitFGs[1],
+      fgMakes: splitFGs[0],
+      timeOfPossession: topSeconds
+    }
+    if (matchNo === 0) {
+      stats.away = teamStats;
+    } else if (matchNo === 1) {
+      stats.home = teamStats;
+    }
+    matchNo++;
+  }
+  return stats;
+};
+
+const parseTeamScores = function parseTeamScores(gameSelfText: string) {
+  const scoreStringsRegex = /Team\|Q1.+?\n.+?\n(.+?)\n(.+?)\n/gm;
+  const scoreStringsMatch = scoreStringsRegex.exec(gameSelfText);
+  if (scoreStringsMatch) {
+    const [, homeScoreString, awayScoreString] = scoreStringsMatch;
+    const parsedScores = [homeScoreString, awayScoreString].map((scoreString) => {
+      return scoreString.split('|').slice(1, -1).map(score => parseInt(score, 10)); // slice to remove team name and final total
+    });
+    return {
+      home: parsedScores[0],
+      away: parsedScores[1]
+    }
+  }
+  return false;
+};
+
 export {
-  parseHomeAway
+  parseHomeAway,
+  parseTeamStats,
+  parseTeamScores
 };
